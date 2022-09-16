b4cb9dbbdc241dda5c82e7c850834da9862c0a07
kitkat145207 <78538147+kitkat145207@users.noreply.github.com>
2022-09-16 18:25:11 +0700

    Update ex4.js

--- ex4.js
+++ ex4.js
@@ -1,3 +1,7 @@
+/*Lấy ra 1 mảng mới từ mảng cho trước sao cho các phần tử chỉ xuất hiện 1 lần (sử dụng Set)
+VD: [1,2,3,1,2,3,4,5,6,4] => [1,2,3,4,5,6]
+*/
 const array = [1, 2, 3, 1, 2, 3, 4, 5, 6, 4];
 const ans = [...new Set(array)];
 console.log(ans)
+    // [ 1, 2, 3, 4, 5, 6 ]
