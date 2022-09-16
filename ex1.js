205ee81d59d1fab24896fbeb08221b4089fb8e6a
kitkat145207 <78538147+kitkat145207@users.noreply.github.com>
2022-09-16 18:24:12 +0700

    Update ex1.js

--- ex1.js
+++ ex1.js
@@ -1,3 +1,9 @@
+/*
+Trả về 1 mảng gồm các phần tử giống nhau trong 2 mảng
+VD: array1 = [1,2,3,4,5,6];
+	array2 = [3,4,8,9,12];
+	=> [3,4]
+*/
 function same(arr1, arr2) {
     var ans = [];
     for (let i of arr1) {
@@ -8,3 +14,9 @@ function same(arr1, arr2) {
     }
     return ans;
 }
+const arr1 = [1, 2, 3, 4, 5, 6];
+const arr2 = [3, 4, 8, 9, 12];
+var newArr = [];
+newArr = same(arr1, arr2);
+console.log(newArr)
+    // [ 3, 4 ]
